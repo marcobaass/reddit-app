93193062358f407bdeefa2653527c076addcdfab
jazcala <jazcalabrese@gmail.com>
2024-12-09 09:34:28 -0300

    Innitial Commit

---
+++ src/components/ArticleList.jsx
@@ -0,0 +1,12 @@
+import Article from './Article';
+
+function ArticleList() {
+  return (
+    <>
+      <h2>Article List</h2>
+      {/* display article list */}
+      <Article />
+    </>
+  )
+}
+export default ArticleList;
