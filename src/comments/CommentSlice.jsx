b0cd37c36705255aba2a52b1b2bf4b8b52b0cd11
Branden Seaberry <brandenseaberry@gmail.com>
2025-01-09 00:53:33 -0600

    File Relocation

--- src/comments/CommentSlice.jsx
+++ src/comments/CommentSlice.jsx
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { baseUrl } from "../shared/baseUrl";
+import { baseUrl } from "../../shared/baseUrl";
 
 
 const initialState = {
