f36f1b8ac66ec983fc333439c95706550c99574e
Marco Baass <picture@marcobaass.com>
2025-01-18 11:17:33 +0100

    searchbar function

---
+++ src/components/searchResults/SearchResults.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import styles from './SearchResults.module.css';
+
+function SearchResults({ results }) {
+  return (
+    <div className={styles['search-results']}>
+      {results.map((result, index) => (
+        <div key={index} className={styles['result-item']}>
+          <h3>{result.title}</h3>
+          <p>{result.selftext}</p>
+          <a href={`https://www.reddit.com${result.permalink}`} target="_blank" rel="noopener noreferrer">
+            View Post
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default SearchResults;
+
+SearchResults.propTypes = {
+  results: PropTypes.arrayOf(PropTypes.object).isRequired,
+}
