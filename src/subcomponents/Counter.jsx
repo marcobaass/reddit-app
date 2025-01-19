4e07d7e3d679a4c4ecb6fe21d13f3a217295a33a
jazcala <jazcalabrese@gmail.com>
2025-01-19 11:24:14 -0300

    Implemented post score and up and down arrow

--- src/subcomponents/Counter.jsx
+++ src/subcomponents/Counter.jsx
@@ -4,14 +4,70 @@
  */
 // TODO add counter logic
 import { Card } from "reactstrap";
-function Counter() {
+import PropTypes from "prop-types";
+import { useState } from "react";
+import CounterButton from "./CounterButton";
+
+function Counter({ score }) {
+  const [votes, setVotes] = useState(score);
+  const [color, setColor] = useState("black");
+
+  const resetScore = () => {
+    setVotes(score);
+    setColor("black");
+  };
+  const onClickUp = () => {
+    if (votes === score) {
+      setVotes(votes + 1);
+      setColor("green");
+      return;
+    }
+    if (votes === score + 1) {
+      resetScore();
+      return;
+    }
+    if (votes === score - 1 && color === "red") {
+      resetScore();
+      return;
+    }
+  };
+
+  const onClickDown = () => {
+    if (votes === score) {
+      setVotes(votes - 1);
+      setColor("red");
+    }
+    if (votes === score - 1) {
+      resetScore();
+      return;
+    }
+    if (votes === score + 1 && color === "green") {
+      resetScore();
+      return;
+    }
+  };
+
   return (
     <Card color="light" outline>
-      <i className="bi bi-caret-up-fill fs-4"></i>
-      <h2 className="my-2">2K</h2>
-      <i className="bi bi-caret-down-fill fs-4"></i>
+      <CounterButton
+        direction="up"
+        handleClick={onClickUp}
+        color={color === "green" ? "green" : "black"}
+      />
+      <h2 className="my-2" style={{ color: color }}>
+        {votes >= 1000 ? `${Math.floor(votes / 1000)}K` : votes}
+      </h2>
+      <CounterButton
+        direction="down"
+        handleClick={onClickDown}
+        color={color === "red" ? "red" : "black"}
+      />
     </Card>
   );
 }
+
+Counter.propTypes = {
+  score: PropTypes.number.isRequired,
+};
 
 export default Counter;
